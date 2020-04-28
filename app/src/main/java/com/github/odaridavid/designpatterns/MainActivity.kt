1e8085c33803df0f574f78b47d80ae63bc7d6950
Davidodari <davidkibzodari@gmail.com>
2020-04-28 19:26:18 +0300

    setup navigation

--- app/src/main/java/com/github/odaridavid/designpatterns/MainActivity.kt
+++ app/src/main/java/com/github/odaridavid/designpatterns/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.github.odaridavid.designpatterns
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.github.odaridavid.designpatterns.abstractfactory.AbstractFactoryActivity
+import com.github.odaridavid.designpatterns.base.BaseActivity
+import com.github.odaridavid.designpatterns.builder.BuilderActivity
+import com.github.odaridavid.designpatterns.factory.FactoryActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), MainNavigator {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun navigateToAbstractFactoryActivity(view: View) =
+        navigateTo<AbstractFactoryActivity>()
+
+    override fun navigateToFactoryActivity(view: View) = navigateTo<FactoryActivity>()
+
+    override fun navigateToBuilderActivity(view: View) = navigateTo<BuilderActivity>()
+
 }
