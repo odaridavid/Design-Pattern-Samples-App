57fa1bd8f7d4d6f2a433bd82e386d29f8ae92d33
Davidodari <davidkibzodari@gmail.com>
2020-06-28 04:01:41 +0300

    Improve sdk utils readability
    Assigns min and max versions to be used for checking
    build versions

--- app/src/main/java/com/github/odaridavid/designpatterns/helpers/SdkUtils.kt
+++ app/src/main/java/com/github/odaridavid/designpatterns/helpers/SdkUtils.kt
@@ -17,13 +17,17 @@ import android.os.Build
 import androidx.annotation.IntRange
 
 internal object SdkUtils {
-    fun versionFrom(@IntRange(from = 1, to = 29) versionCode: Int): Boolean {
-        return Build.VERSION.SDK_INT >= versionCode
-    }
 
-    fun versionUntil(@IntRange(from = 1, to = 29) versionCode: Int): Boolean {
-        return Build.VERSION.SDK_INT <= versionCode
-    }
+    private const val minVersion = Build.VERSION_CODES.LOLLIPOP.toLong()
+    private const val maxVersion = Build.VERSION_CODES.Q.toLong()
+
+    fun versionFrom(@IntRange(from = minVersion, to = maxVersion) versionCode: Int): Boolean =
+        Build.VERSION.SDK_INT >= versionCode
+
+
+    fun versionUntil(@IntRange(from = minVersion, to = maxVersion) versionCode: Int): Boolean =
+        Build.VERSION.SDK_INT <= versionCode
+
 }
 
 
