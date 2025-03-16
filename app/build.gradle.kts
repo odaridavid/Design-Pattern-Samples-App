328294359f66d4576f3816c6cbeb4a8c58698c34
odaridavid <davidkibzodari@gmail.com>
2025-03-16 13:10:06 +0100

    fix dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 }
 
 android {
-    compileSdk = 30
-    buildToolsVersion = "30.0.3"
+    namespace = "com.github.odaridavid.designpatterns"
+    compileSdk = 35
 
     defaultConfig {
         val versionMajor = 1
@@ -17,7 +17,7 @@ android {
 
         applicationId = "com.github.odaridavid.designpatterns"
         minSdk = 21
-        targetSdk = 30
+        targetSdk = 35
         versionCode = versionMajor * 10000 + versionMinor * 100 + versionPatch
         versionName = "$versionMajor.$versionMinor.$versionPatch"
 
@@ -55,37 +55,36 @@ android {
 }
 
 dependencies {
-    val aboutLibrariesVersion = "8.9.1"
-
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.21")
-    implementation("androidx.appcompat:appcompat:1.4.0-alpha03")
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.20")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
     // In-app updates
-    implementation("com.google.android.play:core:1.10.0")
+    implementation("com.google.android.play:app-update:2.1.0")
+    implementation("com.google.android.play:app-update-ktx:2.1.0")
 
     // Material Design
-    implementation("com.google.android.material:material:1.5.0-alpha01")
+    implementation("com.google.android.material:material:1.12.0")
 
     // Libraries used info
-    implementation("com.mikepenz:aboutlibraries-core:$aboutLibrariesVersion")
-    implementation("com.mikepenz:aboutlibraries:$aboutLibrariesVersion")
+    implementation("com.mikepenz:aboutlibraries-core:8.9.1")
+    implementation("com.mikepenz:aboutlibraries:8.9.1")
 
     // Animations
-    implementation("jp.wasabeef:recyclerview-animators:3.0.0")
+    implementation("jp.wasabeef:recyclerview-animators:4.0.2")
 
     // Analytics
-    implementation("com.google.firebase:firebase-analytics:19.0.0")
+    implementation("com.google.firebase:firebase-analytics:22.3.0")
 
     // Crashlytics
-    implementation("com.google.firebase:firebase-crashlytics:18.2.0")
+    implementation("com.google.firebase:firebase-crashlytics:19.4.1")
 
     // Preference
-    implementation("androidx.preference:preference-ktx:1.1.1")
+    implementation("androidx.preference:preference-ktx:1.2.1")
 
     // Markdown Support
-    implementation("us.feras.mdv:markdownview:1.1.0")
+
 
     // Unit testing
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
